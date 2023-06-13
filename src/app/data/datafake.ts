5801fb601087dcedbd4aa0e5e04608d3285f74ff
Rodrigo Souza <rodrigosouza2k@outlook.com>
2023-06-13 17:48:32 -0300

    222

---
+++ src/app/data/datafake.ts
@@ -0,0 +1,14 @@
+export const datafake = [
+  {
+    "id":1,
+    "photo":"aaa",
+    "title":"noticia planetas",
+    "description": "texto"
+  },
+  {
+    "id":2,
+    "photo":"aaa",
+    "title":"noticia planetas",
+    "description": "texto"
+  },
+]
